ce7b8f5d9cbac80f46743399fa412913109e440b
lucianocabelloo <you@example.com>
2024-02-01 10:29:11 -0300

    Fix Import ListColors

--- src/Components/FormColors.jsx
+++ src/Components/FormColors.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormImage from "./FormImage";
-import ListColors from "./listColors";
+import ListColors from "./ListColors";
 
 const FormColors = () => {
   const storedColors = JSON.parse(localStorage.getItem("Colours")) || [];
