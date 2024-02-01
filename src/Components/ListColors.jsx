d5920aff9b42a0013ac802d7457f5e2eda23fe24
lucianocabelloo <you@example.com>
2024-02-01 10:19:30 -0300

    npm run build

--- src/Components/ListColors.jsx
+++ src/Components/ListColors.jsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/prop-types */
 import ItemColors from './ItemColors';
 
 const ListColors = ({ colors, handleDelete }) => {
-  console.log(colors);
   return (
     <div className='w-[100%] h-[100%] bg-red-500 estilo borde'>
       {colors.map((color, index) => (
